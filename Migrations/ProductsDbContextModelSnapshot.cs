3ca7c2399c951b9284e479db9ad1d102223a0b53
Rui Figueiredo <rui.figueiredo@outlook.com>
2016-10-27 23:41:40 +0100

    Setup for Sqlite

---
+++ Migrations/ProductsDbContextModelSnapshot.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using DataAccessTutorial.Web.Models;
+
+namespace DataAccessTutorial.Web.Migrations
+{
+    [DbContext(typeof(ProductsDbContext))]
+    partial class ProductsDbContextModelSnapshot : ModelSnapshot
+    {
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.0.1");
+
+            modelBuilder.Entity("DataAccessTutorial.Web.Models.Product", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Name");
+
+                    b.Property<decimal>("Price");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Products");
+                });
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DataAccessTutorial.Web.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
@@ -14,7 +16,9 @@ namespace DataAccessTutorial.Web
     {
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();             
+            services.AddMvc();   
+            services.AddDbContext<ProductsDbContext>(options =>
+                options.UseSqlite("Data Source=productsDb.sqlite"));
         }
         
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
