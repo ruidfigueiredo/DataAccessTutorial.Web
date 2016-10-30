baf95cb765cc0543427d10e6f10da30ee00d111f
Rui Figueiredo <rui.figueiredo@outlook.com>
2016-10-30 21:48:04 +0000

    Sqlite without migrations

--- Startup.cs
+++ Startup.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DataAccessTutorial.Web.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
@@ -14,16 +16,19 @@ namespace DataAccessTutorial.Web
     {
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();             
+            services.AddMvc();
+            services.AddDbContext<ProductsDbContext>(options => 
+                options.UseSqlite("Data Source=productsDb.sqlite"));             
         }
         
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, ProductsDbContext productsDbContext)
         {
             loggerFactory.AddConsole();
 
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                productsDbContext.Database.EnsureCreated();                
             }
 
             app.UseMvcWithDefaultRoute();
